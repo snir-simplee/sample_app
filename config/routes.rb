7e0a4095c3f7ce196c9b10d7906b2786cf23acf9
snir-simplee <snir@simplee.com>
2018-01-15 10:32:05 +0200

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 SampleApp::Application.routes.draw do
 
+
+  resources :users
+  
   root to: 'static_pages#home'
 
   match '/signup',  to: 'users#new'
@@ -7,7 +10,7 @@ SampleApp::Application.routes.draw do
   match '/about',   to: 'static_pages#about'
   match '/contact', to: 'static_pages#contact'
 
-  get "users/new"
+  # get "users/new"
   get "static_pages/home"
   get "static_pages/help"
   get "static_pages/about"
